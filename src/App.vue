e5d5204962b652f94a84095a621065a4b4991856
lanhai <1444133004@qq.com>
2019-07-23 23:28:44 +0800

    webpack搭建vue环境 下载`npm install -D vue-loader vue-template-compiler` 需配置loader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>我是div</div>
+  <div>
+    <div>我是div</div>
+    <h1>{{msg}}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      msg: "1"
+    };
+  }
 };
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: rebeccapurple;
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 // 导入路径模块
 const path = require("path");
 
+// 导入Vue编译
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
 // 暴露出去
 module.exports = {
   // 打包入口 (表示webpack要打包的文件)
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   // 打包出口 (打包至哪个文件)
   output: {
     // 打包后的文件名
@@ -39,7 +42,16 @@ module.exports = {
             loader: "less-loader" // compiles Less to CSS
           }
         ]
+      },
+      {
+        // 配置vue文件打包
+        test: /\.vue$/,
+        loader: "vue-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin()
+  ]
 };
